1eaaf2724253d16d6e9d35098d93a595f7fb7b84
hibana2077 <hibana2077@gmail.com>
2024-10-26 16:58:53 +0800

    Refactor model and add inference method in VovUnet_Var class

--- src/unet_model.py
+++ src/unet_model.py
@@ -60,6 +60,31 @@ class VovUnet_Var(nn.Module):
         self.up4 = (UpOsa(128, 64, bilinear))
         self.outc = (OutConv(64, n_classes_seg))
 
+    def infrence(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        cls_out = self.cls(x5)
+        return cls_out
+    
+    def cal_params(self):
+        full_params = sum(p.numel() for p in self.parameters())
+        cls_machine_params = 0
+        cls_machine_layers = [
+            self.inc,
+            self.down1,
+            self.down2,
+            self.down3,
+            self.down4,
+            self.cls
+        ]
+        for layer in cls_machine_layers:
+            cls_machine_params += sum(p.numel() for p in layer.parameters())
+        
+        return full_params, cls_machine_params
+
     def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
